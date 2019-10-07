1625d8129f7bdf0d6fa30e12b53c46227dca433e
Tung Lam <tungtobi@gmail.com>
2019-10-07 12:45:22 +0700

    form slider

--- dev/frontend/src/components/HomeBanner.jsx
+++ dev/frontend/src/components/HomeBanner.jsx
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import './HomeBanner.css';
+import './homepage.css';
 import RegisterForm from './RegisterForm';
 import LoginForm from './LoginForm';
-import { Button } from 'react-bootstrap'
+import Slider from 'react-slick';
+import { Carousel } from 'react-bootstrap';
 
 class HomeBanner extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            index: 0,
+            direction: null,
             isLogin: true
         }
 
         this.handleLogin = this.handleLogin.bind(this);
         this.handleRegister = this.handleRegister.bind(this);
+        this.changeState = this.changeState.bind(this);
     }
 
     handleLogin() {
-        this.changeState();
         
     }
 
     handleRegister() {
-        this.changeState();
+
     }
 
     changeState() {
@@ -30,32 +33,69 @@ class HomeBanner extends Component {
         }));
     }
 
+    toggleCarousel = (direction) => {
+        let index = this.state.index;
+    
+        if (direction === 'next')
+          index++
+        else if (direction === 'prev')
+          index--
+        
+        this.setState({
+          direction,
+          index
+        })
+    }
+
     render() {
+        const settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        
         return (
             <section className="home-banner-area">
                 <div className="container">
                     <div className="row justify-content-end fullscreen">
-                        <div className="col-lg-6 col-md-12 home-banner-left d-flex fullscreen">
+                        <div className="col-lg-6 col-md-12 home-banner-left d-flex fullscreen align-items-center">
                             <div>
                                 <h1>
                                     Smart <span className="blue-text">IoT </span><br/>
                                     Room <span className="blue-text">Controller</span>
                                 </h1>
-                                {!this.state.isLogin && 
-                                    <div>
-                                        <h2>Đăng ký</h2>
-                                        <RegisterForm onSubmit={this.handleRegister}/>
-                                </div>}
-                                {this.state.isLogin && 
-                                    <div>
-                                        <div>
-                                            <h2>Đăng nhập</h2>
-                                            <p>hoặc <span className="blue-text">
-                                                <a onClick={this.handleLogin}>đăng ký</a>
-                                            </span></p>
-                                        </div>
-                                        <LoginForm onSubmit={this.handleLogin}/>
-                                </div>}
+                                <Carousel indicators={false} controls={false}
+                                activeIndex={this.state.index} direction={this.state.direction}>
+                                    <Carousel.Item>
+                                        <LoginForm 
+                                            onChangeView={() => this.toggleCarousel('next')} 
+                                            onSubmit={this.handleLogin}
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <RegisterForm 
+                                            onChangeView={() => this.toggleCarousel('prev')} 
+                                            onSubmit={this.handleRegister}
+                                        />
+                                    </Carousel.Item>
+                                </Carousel>
+
+                                {/* {
+                                    !this.state.isLogin && 
+                                    <RegisterForm 
+                                        onChangeView={this.changeState} 
+                                        onSubmit={this.handleRegister}
+                                    />
+                                }
+                                {
+                                    this.state.isLogin && 
+                                    <LoginForm 
+                                        onChangeView={this.changeState} 
+                                        onSubmit={this.handleLogin}
+                                    />
+                                } */}
                             </div>
                         </div>
                         <div className="col-lg-6 col-md-6"></div>

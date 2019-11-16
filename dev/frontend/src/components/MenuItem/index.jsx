671403458aac466cd63d4fca35855abbd58aab79
LinhNV <vietlinhtspt@gmail.com>
2019-11-16 22:59:20 +0700

    Change Card Border, Card Border-radious

--- dev/frontend/src/components/MenuItem/index.jsx
+++ dev/frontend/src/components/MenuItem/index.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
-
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import "./index.css";
 class MenuItem extends Component {
   render() {
     return (
@@ -11,7 +10,7 @@ class MenuItem extends Component {
           onClick={() => this.props.changeWindow(this.props.item.nameItem)}
           variant="primary w-100"
         >
-          <span className="float-left">
+          <span className="float-left font-size-24px">
             <i className={this.props.item.icon}></i>
             <a className="px-xl-2 ">{this.props.item.nameItem}</a>
           </span>

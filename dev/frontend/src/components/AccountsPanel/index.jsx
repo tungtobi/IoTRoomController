8fc12dcdc7c28fa56f838cf51ce7c99d47ff6e72
Tung Lam <tungtobi@gmail.com>
2019-12-17 20:04:57 +0700

    update list users

--- dev/frontend/src/components/AccountsPanel/index.jsx
+++ dev/frontend/src/components/AccountsPanel/index.jsx
@@ -5,36 +5,46 @@ import { Button, Table } from "react-bootstrap";
 class AccountsPanel extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       list: []
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.fetchUserList();
+  }
 
-  // fetchUserList() {
-  //   const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  //   const url = "http://54.237.117.36:3000/admin/list-user";
+  fetchUserList() {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "http://54.237.117.36:3000/admin/list-user";
 
-  //   fetch(proxyurl + url, {
-  //     method: "POST",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json"
-  //     },
-  //     body: JSON.stringify({
-  //       token: localStorage.getItem("token")
-  //     })
-  //   })
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       if (data.error_code === 0) {
+    fetch(proxyurl + url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        token: localStorage.getItem("token")
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.error_code === 0) {
+          let users = [];
+          for (var propName in data) {
+            if (propName.startsWith("user_")) {
+              users.push(data[propName]);
+            }
+          }
 
-  //       }
-  //     })
-  //     .catch(this.handleLoginFailure);
-  // }
+          console.log(users);
+
+          this.setState({ list: users });
+        }
+      })
+      .catch(this.handleLoginFailure);
+  }
   // }
 
   render() {
@@ -46,23 +56,27 @@ class AccountsPanel extends Component {
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col">Device Name</th>
-                <th scope="col">Seria Number</th>
-                <th scope="col">Created Date</th>
-                <th scope="col">Status</th>
-                <th scope="col">Accounts</th>
+                <th scope="col">Username</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Address</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone Number</th>
                 <th scope="col">Action</th>
               </tr>
             </thead>
             <tbody>
-              {this.props.accounts.map(item => (
-                <tr>
-                  <th scope="row">{item.id}</th>
-                  <td>{item.name}</td>
-                  <td>{item.seria}</td>
-                  <td>{item.createdDate}</td>
-                  <td>{item.status}</td>
-                  <td>{item.accounts}</td>
+              {this.state.list.map((item, idx) => (
+                <tr key={idx}>
+                  <th scope="row">{idx + 1}</th>
+                  <td>{item.username}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>{item.gender}</td>
+                  <td>{item.address}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone_number}</td>
                   <td className="p-0">
                     <button
                       onClick={() => this.props.showDevEditModal()}

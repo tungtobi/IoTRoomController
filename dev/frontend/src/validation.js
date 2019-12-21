fb6088977cba74296eb856e494aff7cb9ecedf63
thuy4tbn99 <thuy4tbn99@gmail.com>
2019-12-21 16:58:50 +0700

    validate first_name, lastName

--- dev/frontend/src/validation.js
+++ dev/frontend/src/validation.js
@@ -17,11 +17,19 @@ export default function handleInput(name, value, targetValue = null) {
       break;
     case "email":
       valid = value.match(/^[\@.]$/) ? true : false;
+      break;
     case "phone_number":
       valid = value.match(/^[0-9]$/) ? true : false;
+      break;
     case "address":
       valid = value.match(/^[a-zA-Z0-9.\-_$@*!]{3,30}$/) ? true : false;
-
+      break;
+    case "first_name":
+    	valid = value.match(/^a-zA-Z$/) ?true : false;
+    	break;
+    case "last_name":
+    	valid = value.match(/^a-zA-Z$/) ?true : false;
+    	break;
     default:
       return null;
   }

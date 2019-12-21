55994510037903a488b33ea8bb35c6c989549e28
thuy4tbn99 <thuy4tbn99@gmail.com>
2019-12-21 18:31:59 +0700

    fix validation

--- dev/frontend/src/validation.js
+++ dev/frontend/src/validation.js
@@ -16,19 +16,19 @@ export default function handleInput(name, value, targetValue = null) {
       valid = value.match(/^[a-zA-Z0-9 ]{3,30}$/) ? true : false;
       break;
     case "email":
-      valid = value.match(/^[\@.]$/) ? true : false;
+      valid = value.match(/\@./) ? true : false;
       break;
     case "phone_number":
-      valid = value.match(/^[0-9]$/) ? true : false;
+      valid = value.match(/\D/) ? false : true;
       break;
     case "address":
-      valid = value.match(/^[a-zA-Z0-9.\-_$@*!]{3,30}$/) ? true : false;
+      valid = value.match(/^[\S]+(\s[\S]+)*$/) ? true : false;
       break;
     case "first_name":
-    	valid = value.match(/^a-zA-Z$/) ?true : false;
+    	valid = value.match(/^[a-zA-Z]*$/) ?true : false;
     	break;
     case "last_name":
-    	valid = value.match(/^a-zA-Z$/) ?true : false;
+    	valid = value.match(/^[a-zA-Z]*$/) ?true : false;
     	break;
     default:
       return null;
